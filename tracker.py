2a382691fdd617a84e41027b51b22c8c9c139efb
Die4Ever <die4ever2005@gmail.com>
2024-09-04 03:55:32 -0500

    made this actually work

--- tracker.py
+++ tracker.py
@@ -4,33 +4,53 @@ from pathlib import Path
 import json
 import re
 
-FONT_NAME="CourierPrimeCode.ttf"
-FONT_SIZE = 28
+FONT_NAME = "CourierPrimeCode.ttf"
+FONT_SIZE = 45
 IMAGE_WIDTH = 1920
 IMAGE_HEIGHT = 1080
+START_X = 710
+START_Y = 160
+COL_WIDTH = 340
 
-augs = set(('Speed Enhancement', 'Power Recirculator'))
-skills = set()
+important_augs = ('Speed Enhancement', 'Power Recirculator')
+short_augs =     ('Speed',             'Power Recirc'      )
+all_skills =     ('Weapons: Heavy', 'Weapons: Pistol', 'Weapons: Rifle', 'Weapons: Low-Tech', 'Weapons: Demolition', 'Environmental Training', 'Lockpicking', 'Electronics', 'Medicine', 'Computer', 'Swimming')
+short_skills =   ('Heavy',          'Pistol',          'Rifle',          'Low-Tech',          'Demolition',          'Enviro',                 'Lockpicking', 'Electronics', 'Medicine', 'Computer', 'Swimming')
 
-skill_levels = ['UNTRAINED', 'TRAINED', 'ADVANCED', 'MASTER']
+skill_levels =   ['UNTRAINED', 'TRAINED', 'ADVANCED', 'MASTER']
 
 players = {
     'Nitram': 'testlog.txt',
-    #'Ramisme': {},
-    #'Voukras': {},
+    'Ramisme': 'testlog.txt',
+    'Voukras': 'testlog.txt',
 }
 
 font = ImageFont.truetype(FONT_NAME, FONT_SIZE)
-Path('out').mkdir(exist_ok=True)
+
+def main():
+    Path('out').mkdir(exist_ok=True)
+    MakeLayout()
+    idx = 0
+    for (k,v) in players.items():
+        handlePlayer(idx, k, v)
+        idx += 1
+
+
 
 def handlePlayer(idx:int, player:str, logpath:str):
     state_changes = {}
     logtext = Path(logpath).read_text()
     states = {}
     for line in logtext.splitlines():
-        (timestamp, newstates) = checkLogLine(line, states)
+        ret = checkLogLine(line, states)
+        if not ret:
+            continue
+        (timestamp, newstates) = ret
         if not newstates or newstates == states:
             continue
+        #print('')
+        #print('')
+        #print(line)
         #print(timestamp, 'newstates == ', newstates)
         states = newstates
         state_changes[timestamp] = states
@@ -40,51 +60,85 @@ def handlePlayer(idx:int, player:str, logpath:str):
         MakePlayerImage(idx, player, timestamp, state)
 
 
-
-def checkLogLine(line:str, states:dict):
+def parseUpgrade(line:str, states:dict):
     global skill_levels
-    # skills/augs are mostly interchangeable here
-    m = re.match(r'ClientMessage: (?P<skill>.+) upgraded to (?P<level>\w+) (?P<timestamp>[\d:]+)', line)
-    if m:
-        #print(m.groupdict())
-        states = states.copy()
-        timestamp = m.group('timestamp')
-        level = skill_levels.index(m.group('level'))
-        states[m.group('skill')] = level
-        return (timestamp, states)
-    
-    m = re.match(r'DXRStats: (?P<timestamp>[\d:]+) (skills|augs): (?P<skills>.+)', line)
+    # skills/augs are interchangeable here
+    m = re.match(r'.*: ClientMessage: (?P<name>.+) upgraded to ((?P<levelname>\w+)|(level (?P<levelnum>\d))) \(from .* to .*\) (?P<timestamp>[\d:\.]+)', line)
     if not m:
         return None
     #print(m.groupdict())
-    levels = m.group('skills').split(', ')
-    if not levels:
-        return None
     states = states.copy()
-    for namelvl in levels:
-        (name, lvl) = namelvl.split(':')
-        states[name] = lvl
-        skills.add(name)
     timestamp = m.group('timestamp')
+    levelname = m.group('levelname')
+    levelnum = m.group('levelnum')
+    if levelname:
+        level = skill_levels.index(levelname)
+    else:
+        level = int(levelnum) - 1
+    states[m.group('name')] = level
     return (timestamp, states)
+
+
+def parseAugInstall(line:str, states:dict):
+    # based on vanilla AugNowHaveAtLevel, we should improve that so we can get the aug strength
+    m = re.match(r'.*: ClientMessage: Augmentation (?P<aug>.+) at level 1 (?P<timestamp>[\d:\.]+)', line)
+    if not m:
+        return None
+    #print(m.groupdict())
+    states = states.copy()
+    timestamp = m.group('timestamp')
+    states[m.group('aug')] = 0
+    return (timestamp, states)
+
+
+def parseAnyEntry(line:str, states:dict):
+    m = re.match(r'DXRStats: PlayerAnyEntry (?P<timestamp>[\d:\.]+) skills/augs: (?P<skills>.+)', line)
+    if not m:
+        return None
+    #print(line)
+    #print(m.groupdict())
+    timestamp = m.group('timestamp')
+    skillslist = m.group('skills').split(', ')
+    if not skillslist:
+        return None
+    states = dict() # states.copy() # start fresh, to delete no-longer existing augs
+    for namelvl in skillslist:
+        m = re.match(r'(.+):(\d)', namelvl)
+        name = m.group(1)
+        lvl = m.group(2)
+        states[name] = int(lvl)
+    return (timestamp, states)
+
+
+def checkLogLine(line:str, states:dict):
+    ret = parseUpgrade(line, states)
+    if ret:
+        return ret
+
+    ret = parseAugInstall(line, states)
+    if ret:
+        return ret
+    
+    return parseAnyEntry(line, states)
 
 
 
 def MakeLayout():
-    global augs, skills
+    global short_augs, short_skills
     img = Image.open('baselayout.png')
+    assert img.width == IMAGE_WIDTH
+    assert img.height == IMAGE_HEIGHT
     gc = ImageDraw.Draw(img)
-    text = '\n'.join(augs)
-    if augs:
+    text = '\n'.join(short_augs)
+    if short_augs:
         text += '\n'
-    text += '\n'.join(skills)
-    DrawColumn(gc, 0, '', text)
+    text += '\n'.join(short_skills)
+    DrawColumn(gc, -1.2, '', text) # HACK: this column -1.2 for better positioning
     outname = Path('out') / 'layout.png'
     img.save(outname)
 
 
 def MakePlayerImage(col, player, timestamp, state):
-    #img = Image.open('layout.png')
     img = Image.new("RGBA", (IMAGE_WIDTH, IMAGE_HEIGHT))#, 'black')
     gc = ImageDraw.Draw(img)
     DrawPlayer(gc, col, player, state)
@@ -92,29 +146,29 @@ def MakePlayerImage(col, player, timestamp, state):
     outpath = Path('out') / outname
     img.save(outpath)
 
-def DrawPlayer(gc, col, name, state):
-    global augs, skills
+def DrawPlayer(gc, col, name, state:dict):
+    global important_augs, all_skills
     text = ''
-    for aug in augs:
-        lvl = state.get(aug, '')
-        text += str(lvl) + '\n'
-    for skill in skills:
+    for aug in important_augs:
+        lvl = state.get(aug)
+        if lvl is None:
+            text += 'N/A\n'
+            continue
+        text += str(lvl+1) + '\n'
+    for skill in all_skills:
         lvl = state.get(skill, 0)
         text += skill_levels[int(lvl)] + '\n'
-    DrawColumn(gc, col, name, text)
+    print(text)
+    DrawColumn(gc, col, name, text, align='center')
 
 
-def DrawColumn(gc, col, header, text):
-    coords = (col * 100, 10)
+def DrawColumn(gc:ImageDraw.ImageDraw, col, header, text, align='left'):
+    coords = (START_X + col * COL_WIDTH, START_Y)
+    print('DrawColumn', header)
+    print(text)
     text = header + '\n' + text
-    gc.text(coords, text, font=font)
+    gc.text(coords, text, font=font, align=align, spacing=30)
 
 
-
-
-idx = 1
-for (k,v) in players.items():
-    handlePlayer(idx, k, v)
-    idx += 1
-MakeLayout()
-
+if __name__ == "__main__":
+    main()
